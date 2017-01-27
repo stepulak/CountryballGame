39e13a40c9ea894d4f0de21c9a2ae856cf3b525d
Stepan Trcka <savage2@email.cz>
2017-01-27 23:26:22 +0100

    gamepad

--- game/VirtualGamepad.lua
+++ game/VirtualGamepad.lua
@@ -4,82 +4,90 @@ require "GuiElement"
 local VirtualGamepadOffset = 20
 local VirtualGamepadPressColorQ = 1.5
 
-local VirtualGamepadDirButColor = {
+local VirtualGamepadButColor = {
 	r = 0,
 	g = 51,
 	b = 102,
 	a = 255,
 }
 
--- It consist from three sections:
--- *Main direction button (left side of the screen)
--- *A button (right side)
--- *B button (right side)
---
--- And FYI the mouse keyword here refers to touch keyword, because you can
+-- Gamepad consist of direction joystick and N buttons (A, B, C, ...)
+-- FYI the mouse keyword here refers to touch keyword, because you can
 -- use this gamepad with both mouse and touch screen...
-
 VirtualGamepad = GuiElement:new()
 
-function VirtualGamepad:new(virtScrWidth, virtScrHeight, buttonSize, buttonTex)
+function VirtualGamepad:new(virtScrWidth, virtScrHeight, buttonRad, buttonTex)
 	self:super("virtual_gamepad", 0, 0, 0, 0)
 	
+	self.virtScrWidth = virtScrWidth
+	self.virtScrHeight = virtScrHeight
+	self.buttonRad = buttonRad
 	self.buttonTex = buttonTex
-	self.buttonSize = buttonSize
 	
 	-- Direction button
-	self.dirButton = {}
-	self.dirButton.size = buttonSize * 2
-	self.dirButton.x = VirtualGamepadOffset + self.dirButton.size/2
-	self.dirButton.y = virtScrHeight - VirtualGamepadOffset -
-		self.dirButton.size/2
+	self.dirBut = {
+		radius = buttonRad * 2
+	}
+	self.dirBut.x = VirtualGamepadOffset + self.dirBut.radius/2
+	self.dirBut.y = virtScrHeight - VirtualGamepadOffset -
+		self.dirBut.radius/2
 	
-	-- Small button inside
-	self.dirButton.smX = self.dirButton.x
-	self.dirButton.smY = self.dirButton.y
+	-- direction button of "joystick"
+	self.dirBut.smX = self.dirBut.x
+	self.dirBut.smY = self.dirBut.y
+	
+	self.buttons = {}
+end
+
+function VirtualGamepad:getNewHorizontalPosButton()
+	if #self.buttons == 0 then
+		return self.virtScrWidth - VirtualGamepadOffset
+	else
+		local b = self.buttons[#self.buttons]
+		return b.x - b.radius/2 - VirtualGamepadOffset
+	end
+end
+
+-- @label should be one-character-long (doesn't matter actually,
+-- first character is taken anyway)
+-- @actionPressed trigger func when button is pressed
+-- @actionReleased trigger func when button is released
+function VirtualGamepad:addButton(label, actionPressed, actionReleased)
+	local but = {}
+	
+	but.label = string.sub(label, 1)
+	but.actionPressed = actionPressed
+	but.actionReleased = actionReleased
+	but.radius = self.buttonRad
+	but.x = self:getNewHorizontalPosButton() - but.radius/2
+	but.y = self.virtScrHeight - VirtualGamepadOffset
+	
+	self.buttons[#self.buttons + 1] = but
 end
 
 function VirtualGamepad:mouseInsideButton(x, y, but)
 	local dX = x - but.x
 	local dY = y - but.y
-	return math.sqrt(dX*dX + dY*dY) < but.size
+	return math.sqrt(dX*dX + dY*dY) < but.radius
 end
 
 function VirtualGamepad:mouseInArea(x, y)
-	return self:mouseInsideButton(x, y, self.dirButton) or
-		self:mouseInsideButton(x, y, self.aButton) or
-		self:mouseInsideButton(x, y, self.bButton)
+	return self:mouseInsideButton(x, y, self.dirBut)
 end
 
 -- Mouse click = touch down
 function VirtualGamepad:mouseClick(x, y)
-
 end
 
 -- Touch up
 function VirtualGamepad:mouseRelease(x, y)
-
 end
 
 function VirtualGamepad:mouseReleaseNotInside(x, y)
-
 end
 
 function VirtualGamepad:mouseMove(x, y, distX, distY)
-
-end
-
-function VirtualGamepad:drawDirectionButton()
-	-- Border
-	love.graphics.setLineWidth(3)
-	love.graphics.setColor(0, 0, 0)
-	love.graphics.circle("line", self.dirButton.x, self.dirButton.y,
-		self.size, 50)
-	love.graphics.setColor(255, 255, 255)
-	
-	-- Small button inside
 end
 
 function VirtualGamepad:draw()
-	self:drawDirectionButton()
 end
